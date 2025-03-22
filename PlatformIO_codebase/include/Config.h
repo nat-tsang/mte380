2bfc19c10cfd2c856c06654e5f120e99d2130c87
chelseadmytryk <cdmytryk@uwaterloo.ca>
2025-03-22 15:13:51 -0400

    first script to test encoders

---
+++ PlatformIO_codebase/include/Config.h
@@ -0,0 +1,15 @@
+#pragma once // “Include this file only once when compiling, even if it’s included multiple times elsewhere.”
+
+// Motor pins
+const int LEFT_IN2 = 6;  // PWM2
+const int LEFT_IN1 = 7;  // PWM3
+const int RIGHT_IN2 = 8; // PWM4
+const int RIGHT_IN1 = 9; // PWM5
+
+// Encoder Pins
+// Moving fwd will increase encoder count
+const int ENCODER_IN3 = 16; // Drive 2 connected to U3
+const int ENCODER_IN4 = 17; // Drive 2
+const int ENCODER_IN5 = 14; // Drive 1 NEW PINS (right) connected to U2
+const int ENCODER_IN6 = 21; // Drive 1 NEW PINS
+
