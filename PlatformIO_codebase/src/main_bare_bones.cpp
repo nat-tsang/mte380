35926142d3bbea7cefd76aad46eb2a6b74598a55
chelseadmytryk <cdmytryk@uwaterloo.ca>
2025-03-26 22:42:38 -0400

    new main...let's see

--- PlatformIO_codebase/src/main_bare_bones.cpp
+++ PlatformIO_codebase/src/main_bare_bones.cpp
@@ -11,15 +11,14 @@
 #include <PixyLineTracker.h>
 #include <PIDController.h>
 #include <Helpers.h>
-
-<<<<<<<< HEAD:PlatformIO_codebase/src/main_bare_bones.cpp
-========
+#include <Filter.h>
+#include <TurnController.h>
+#include <EncoderReader.h>
 #define BTSerial Serial1
 
 EncoderReader rightEncoder(ENCODER_IN5, ENCODER_IN6);
 EncoderReader leftEncoder(ENCODER_IN3, ENCODER_IN4);
 
->>>>>>>> 8b2815d423c0a5c227ca81a2a890beaae69a6dab:PlatformIO_codebase/src/main_switch_cases.cpp.bak
 Motor rightMotor(u2_IN1, u2_IN2, true);
 Motor leftMotor(u3_IN1, u3_IN2, false);
 
@@ -32,35 +31,203 @@ PIDController linePID(LINE_KP, LINE_KI, LINE_KD);
 PixyLineTracker lineTracker; // Pixy object for line, bullseye and legoman detection
 Pixy2 pixy;
 
-<<<<<<<< HEAD:PlatformIO_codebase/src/main_bare_bones.cpp
+Filter<float, 3> pixyErrorFilter;    // For Pixy X-position (float)
+
 int rightbasePWM = 70;  // Base PWM value
 int leftbasePWM = 70;  // Base PWM value
-========
-Filter<float, 3> pixyErrorFilter;    // For Pixy X-position (float)
-Filter<float, 3> speedFilter;       // For encoder speeds (float)
+bool hasTurned = false;
 
 TurnController turnController(leftMotor, rightMotor, leftEncoder, rightEncoder, WHEEL_BASE, WHEEL_DIAMETER);
-
-float targetVelocity = 1.3;  // m/s forward speed
-float rightbasePWM = 70;  // Base PWM value
-float leftbasePWM = 70;  // Base PWM value
->>>>>>>> 8b2815d423c0a5c227ca81a2a890beaae69a6dab:PlatformIO_codebase/src/main_switch_cases.cpp.bak
-bool hasTurned = false;
 
 enum PiddyState {
   LINE_FOLLOWING,
   BULLSEYE_DETECT,
-  // LEGOMAN_ALIGN,
-  // PICKUP_LEGOMAN,
+  LEGOMAN_ALIGN,
+  PICKUP_LEGOMAN,
   IDLE
 };
 
 PiddyState currentState = IDLE;
 
+// === Function Prototype ===
+void printVoltageLevel(int analogPin);
+void debugPrint(String msg);
+bool legoManAlign(int thresholdX, int thresholdY, const Block* block, int numBlock);
+float checkBatteries(int pin);
+void flashPixyLight(int times);
+
+
+void setup() {
+  Serial.begin(115200);
+  BTSerial.begin(9600);
+
+  gripper.attach(); // Attach servo at startup
+  gripper.close();   // Close gripper
+
+  initButton(START_SIG);  // Initialize button pin
+  linePID.reset();
+  //fan.turnOn(); // Turn fan on at startup
+
+  lineTracker.begin();  // Initialize Pixy
+  lineTracker.setLampOFF();
+
+  pinMode(BATTERY_LEVEL_PIN, INPUT); // Battery level
+
+  debugPrint("Code uploaded. STARTING ROUTINE");
+  checkBatteries(BATTERY_LEVEL_PIN);  // Check battery voltage
+  delay(1000);
+}
+
+
+void loop() {
+  if (SYSTEM_SHUTDOWN) return;  // Halt program here if shutdown triggered
+  // === Read battery voltage ===
+  checkBatteries(BATTERY_LEVEL_PIN);  // Check battery voltage
+  
+  static bool lastRobotRunning = false;
+  // === Check button state ===
+  checkButton(leftMotor, rightMotor);  // Check button state and toggle robotRunning state
+
+  // === State toggle logic ===
+  if (robotRunning != lastRobotRunning) { // If button state changes
+    if (robotRunning) {
+      debugPrint("Switching to LINE_FOLLOWING");
+      gripper.close();  // Optional: ready for pickup
+      currentState = LINE_FOLLOWING;
+    } else {
+      debugPrint("Switching to IDLE");
+      hasTurned = false;
+      currentState = IDLE;
+    }
+  }
+  lastRobotRunning = robotRunning;
+
+  // if (robotRunning && currentState == IDLE) {
+  //   gripper.close();
+  //   currentState = LINE_FOLLOWING;
+  // } else if (!robotRunning && currentState != IDLE) {
+  //   hasTurned = false;
+  //   currentState = IDLE;
+  // }
+
+  pixy.ccc.getBlocks();
+  const auto* blocks = pixy.ccc.blocks;
+  int numBlocks = pixy.ccc.numBlocks;
+
+  switch (currentState) {
+    case IDLE:
+      leftMotor.stop();
+      rightMotor.stop();
+      break; 
+
+    case LINE_FOLLOWING: {
+      float pixyError = lineTracker.readLinePosition(blocks, numBlocks);  // +157.5 (far left drift) to -157.5 (far right drift)
+      lineTracker.findBullseye(100, 35, 50, 20, blocks, numBlocks); // currently on nightime, 175, 50, 30, 20 (daytime) 175, 50, 50, 20 (more forgiving x)
+      if (lineTracker.getBullseye()) {
+        leftMotor.stop();
+        rightMotor.stop();
+        debugPrint("Bullseye found in stopping range.");
+        currentState = LEGOMAN_ALIGN;
+        break;
+      }
+
+      if (!lineTracker.getLineDetected()) {
+        debugPrint("No line seen");
+        leftMotor.stop();
+        rightMotor.stop();
+        linePID.reset();
+        pixyErrorFilter.reset();
+        currentState = IDLE;
+        break; 
+      }
+
+      if (abs(pixyError) < 10.0) {
+        pixyError = 0;
+      }
+
+      double steeringCorrection = linePID.compute(pixyError);  // Output is differential m/s, -ve means turn left, +ve means turn right
+
+      int leftPWM = constrain(leftbasePWM + steeringCorrection, 0, 150);
+      int rightPWM = constrain(rightbasePWM - steeringCorrection, 0, 150);
+      
+      // === Apply Motor Commands ===
+      leftMotor.setSpeed(leftPWM);
+      rightMotor.setSpeed(rightPWM);  
+      break;
+    }
+
+    case LEGOMAN_ALIGN: {
+      if (legoManAlign(30, 145, blocks, numBlocks)) {
+        debugPrint("Legoman centered. ");
+        currentState = PICKUP_LEGOMAN;
+      }
+      break;
+    }
+
+    case PICKUP_LEGOMAN: {
+      gripper.close();
+      debugPrint("Turning 180 with legoman. ");
+      // delay(1000); // debouncing, allows gripper to fully close 
+      if (!hasTurned) {
+        turnController.turnDegrees(180, 70); // 70 from testing in driveAndTurn.cpp
+        hasTurned = true;
+      }
+      // leftMotor.stop();
+      // rightMotor.stop();
+      currentState = LINE_FOLLOWING;
+      break;
+    }
+    default:
+      // safety catch 
+      leftMotor.stop();
+      rightMotor.stop();
+      currentState = IDLE;
+      break;
+  }
+}
+
+
+// === Reads actual voltage using analog pin and divider ===
+float checkBatteries(int pin) {
+  static unsigned long lastPrintTime = 0;
+  unsigned long currentTime = millis();
+
+  float batteryVoltage = ((analogRead(pin)/ANALOG_RESOLUTION) * 3.3)/0.3197;
+
+  // Only print every 5000 ms (5 seconds)
+  if (currentTime - lastPrintTime >= 5000) {
+    debugPrint("Battery Voltage: " + String(batteryVoltage));
+    lastPrintTime = currentTime;
+  }
+
+  if (batteryVoltage < BATTERY_VOLTAGE_THRESHOLD) {
+    debugPrint("Battery low. SYSTEM SHUTDOWN");
+    SYSTEM_SHUTDOWN = true;
+    leftMotor.stop();
+    rightMotor.stop();
+    fan.turnOff();
+    flashPixyLight(5);
+  }
+  return batteryVoltage;
+}
+
+
+// === Flashes Pixy cam lamp ===
+void flashPixyLight(int times) {
+  for (int i = 0; i < times; i++) {
+    pixy.setLamp(1, 1); // LEDs ON
+    delay(200);
+    pixy.setLamp(0, 0); // LEDs OFF
+    delay(200);
+  }
+}
+
+
 void debugPrint(String msg) {
   Serial.println(msg);
   BTSerial.println(msg);
 }
+
 
 bool legoManAlign(int thresholdX, int thresholdY, const Block* block, int numBlock) {
   auto [x, y] = lineTracker.getPixyCoord(6, block, numBlock); // orange shayla is 6
@@ -88,162 +255,4 @@ bool legoManAlign(int thresholdX, int thresholdY, const Block* block, int numBlo
     rightMotor.stop();
   }
   return false;
-}
-
-void setup() {
-  Serial.begin(115200);
-  gripper.attach(); // Attach servo at startup
-  gripper.close();   // Close gripper
-  initButton(START_SIG);  // Initialize button pin
-
-  linePID.reset();
-  fan.turnOff(); // Turn fan off at startup
-  lineTracker.begin();
-  lineTracker.setLampOFF();
-  debugPrint("Code uploaded. ");
-}
-
-void loop() {
-  checkButton(leftMotor, rightMotor);  // Check button state and toggle robotRunning state
-<<<<<<<< HEAD:PlatformIO_codebase/src/main_bare_bones.cpp
-    if (robotRunning && currentState == IDLE) {
-      gripper.close();
-      currentState = LINE_FOLLOWING;
-    } else if (!robotRunning && currentState != IDLE) {
-      hasTurned = false;
-      lineTracker.setLampOFF();
-      currentState = IDLE;
-    }
-========
-  if (robotRunning && currentState == IDLE) {
-    gripper.open();
-    currentState = LINE_FOLLOWING;
-  } else if (!robotRunning && currentState != IDLE) {
-    hasTurned = false;
-    linePID.reset();
-    pixyErrorFilter.reset();
-    currentState = IDLE;
-  }
->>>>>>>> 8b2815d423c0a5c227ca81a2a890beaae69a6dab:PlatformIO_codebase/src/main_switch_cases.cpp.bak
-
-  pixy.ccc.getBlocks();
-  const auto* blocks = pixy.ccc.blocks;
-  int numBlocks = pixy.ccc.numBlocks;
-
-  switch (currentState) {
-    case IDLE:
-      leftMotor.stop();
-      rightMotor.stop();
-      break; 
-
-    case LINE_FOLLOWING: {
-<<<<<<<< HEAD:PlatformIO_codebase/src/main_bare_bones.cpp
-      // Serial.println("Line following now. ");
-      float pixyError = lineTracker.readLinePosition();  // +157.5 (far left drift) to -157.5 (far right drift)
-
-      lineTracker.findBullseye(100, 40, 30, 20); // currently on nightime, 175, 50, 30, 20 (daytime) 175, 50, 50, 20 (more forgiving x)
-      if (lineTracker.isBullseye()) {
-        leftMotor.stop();
-        rightMotor.stop();
-        Serial.println("Bullseye found in stopping range.");
-        currentState = IDLE;
-        break;
-      }
-
-      if (!lineTracker.isLineDetected()) {
-        Serial.println("No line seen");
-========
-      float pixyError = lineTracker.readLinePosition(blocks, numBlocks);  // +157.5 (far left drift) to -157.5 (far right drift)
-      
-      lineTracker.findBullseye(100, 35, 50, 20, blocks, numBlocks); // currently on nightime, 175, 50, 30, 20 (daytime) 175, 50, 50, 20 (more forgiving x)
-      if (lineTracker.getBullseye()) {
-        leftMotor.stop();
-        rightMotor.stop();
-        debugPrint("Bullseye found in stopping range.");
-        currentState = LEGOMAN_ALIGN;
-        break;
-      }
-
-      if (!lineTracker.getLineDetected()) {
-        debugPrint("No line seen");
-        leftMotor.stop();
-        rightMotor.stop();
-        linePID.reset();
-        pixyErrorFilter.reset();
->>>>>>>> 8b2815d423c0a5c227ca81a2a890beaae69a6dab:PlatformIO_codebase/src/main_switch_cases.cpp.bak
-        currentState = IDLE;
-        break; 
-      }
-
-      if (abs(pixyError) < 10.0) {
-        pixyError = 0;
-      }
-
-      double steeringCorrection = linePID.compute(pixyError);  // Output is differential m/s, -ve means turn left, +ve means turn right
-
-      int leftPWM = constrain(leftbasePWM + steeringCorrection, 0, 150);
-      int rightPWM = constrain(rightbasePWM - steeringCorrection, 0, 150);
-      
-      // === Apply Motor Commands ===
-      leftMotor.setSpeed(leftPWM);
-<<<<<<<< HEAD:PlatformIO_codebase/src/main_bare_bones.cpp
-      rightMotor.setSpeed(rightPWM);
-      break;
-    }
-
-    // case LEGOMAN_ALIGN: {
-    //   // lineTracker.setLampON();
-    //   if (legoManAlign(30, 145)) {
-    //     Serial.println("Legoman centered. ");
-    //     // lineTracker.setLampOFF();
-    //     currentState = PICKUP_LEGOMAN;
-    //   }
-    //   break;
-    // }
-    // case PICKUP_LEGOMAN: {
-    //   gripper.close();
-    //   // delay(1000); // debouncing, allows gripper to fully close 
-    //   if (!hasTurned) {
-    //     turnController.turnDegrees(180, 70); // 70 from testing in driveAndTurn.cpp
-    //     hasTurned = true;
-    //   }
-    //   // if the above turn has problems, definitely will need to edit turnController to turn until red line is found again or smth
-    //   // currentState = IDLE;
-    //   lineTracker.setLampON();
-    //   leftMotor.stop();
-    //   rightMotor.stop();
-    //   break;
-    // }
-========
-      rightMotor.setSpeed(rightPWM);  
-      break;
-    }
-
-    case LEGOMAN_ALIGN: {
-      if (legoManAlign(30, 145, blocks, numBlocks)) {
-        debugPrint("Legoman centered. ");
-        currentState = PICKUP_LEGOMAN;
-      }
-      break;
-    }
-    case PICKUP_LEGOMAN: {
-      gripper.close();
-      debugPrint("Turning 180 with legoman. ");
-      // delay(1000); // debouncing, allows gripper to fully close 
-      if (!hasTurned) {
-        turnController.turnDegrees(180, 70); // 70 from testing in driveAndTurn.cpp
-        hasTurned = true;
-      }
-      leftMotor.stop();
-      rightMotor.stop();
-      break;
-    }
->>>>>>>> 8b2815d423c0a5c227ca81a2a890beaae69a6dab:PlatformIO_codebase/src/main_switch_cases.cpp.bak
-    default:
-      // safety catch 
-      leftMotor.stop();
-      rightMotor.stop();
-      currentState = IDLE;
-      break;
-  }
 }
